e8c1b2b3b9c90ad5e9b920ca87e7a8482bd00d8e
Dave Van Wagner <git@davevw.com>
2024-07-10 18:21:56 -0700

    fix directory load address

--- src/6502/build.sh
+++ src/6502/build.sh
@@ -3,5 +3,5 @@ export ACME=${USERPROFILE}/Downloads/acme0.97win/acme
 export VICE=${USERPROFILE}/Downloads/GTK3VICE-3.8-win64/bin
 export PROG=vwas6502
 ${ACME}/acme -f cbm -o ${PROG}.prg -l ${PROG}.lbl -r ${PROG}.lst ${PROG}.asm \
-&& ${VICE}/c1541 ${PROG}.d64 -attach ${PROG}.d64 8 -delete "${PROG} 52224" -write ${PROG}.prg "${PROG} 52224" \
+&& ${VICE}/c1541 ${PROG}.d64 -attach ${PROG}.d64 8 -delete "${PROG} 49152" -write ${PROG}.prg "${PROG} 49152" \
 && ${VICE}/x64sc -moncommands ${PROG}.lbl -autostart ${PROG}.d64 >/dev/null 2>&1 &
